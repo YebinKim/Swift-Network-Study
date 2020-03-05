980c1aaa30d8f01ba0a4d80088b52112962c7294
YebinKim <yb2316@naver.com>
2020-03-05 19:53:06 +0900

    :tada: Create KingFisher example app

---
+++ KingFisher/ArtworkOnItunes/ArtworkOnItunes/View Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtworkOnItunes
+//
+//  Created by Yebin Kim on 2020/03/05.
+//  Copyright © 2020 김예빈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
